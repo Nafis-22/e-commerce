c589dd84f780d0a7e1748a4b729c9c6eabb6534c
Nafis-22 <98020183+Nafis-22@users.noreply.github.com>
2022-08-04 00:31:33 +0530

    Add files via upload

---
+++ build/precache-manifest.ca9e39a2530bf3f9dabe0ffa5e5a59f2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f5ce276ee1a0a4ae0c3ba1e8462e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54482da2cfba422bedf6",
+    "url": "/static/css/2.f9bc93bb.chunk.css"
+  },
+  {
+    "revision": "eb476c5b508e77ad36d0",
+    "url": "/static/css/main.8ae0b6ed.chunk.css"
+  },
+  {
+    "revision": "54482da2cfba422bedf6",
+    "url": "/static/js/2.1e426418.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1e426418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb476c5b508e77ad36d0",
+    "url": "/static/js/main.874b90f8.chunk.js"
+  },
+  {
+    "revision": "e6ca908af9d4fe008ad3",
+    "url": "/static/js/runtime-main.9ef4c40e.js"
+  },
+  {
+    "revision": "07505306d3544c2687eed549f1c61cd8",
+    "url": "/static/media/bbb.07505306.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/static/media/watch.b4e58e91.jpg"
+  }
+]);
